bccef74f8304ad7fcf82552e52972d327303f701
ViankaMB <54475761+ViankaMB@users.noreply.github.com>
2020-04-15 15:23:19 -0400

    Place data on webpage
    La data obtenida de la API la colocamos para que sea vista en nuestra página web

--- weather.js
+++ weather.js
@@ -8,5 +8,16 @@ $.ajax ({
         console.log(result);
         console.log(result.name);
 
+        $("#location").text(result.name);
+
+        const C = Math.round(result.main.temp - 273.15);
+        const Celsius = C.toString();
+        $("#temperature").text(Celsius);
+
+        const windSpeed = Math.round(result.wind.speed * 1.60934);
+        const ws = windSpeed.toString();
+        $("#windSpeed").text(ws);
+
+        $("#sky").text(result.weather[0].description);
     }
 })
