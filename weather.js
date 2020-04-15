82f4b6acfcb3af61f9b0fb8ae3c640274b1b4d64
ViankaMB <54475761+ViankaMB@users.noreply.github.com>
2020-04-15 16:39:02 -0400

    City changed

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const url = "http://api.openweathermap.org/data/2.5/weather?q=Santo Domingo&appid=b9041d1d914c8697e543d1f47bedd975";
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Santo+Domingo&appid=b9041d1d914c8697e543d1f47bedd975";
 
 $.ajax ({
     url: url,
