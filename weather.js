2d4a629e9e8aab36db7cc83c78e68b118403569d
ViankaMB <54475761+ViankaMB@users.noreply.github.com>
2020-04-15 14:27:25 -0400

    Retrieve data
    Se agregó la funcionalidad de jQuery.
    
    Obtuvimos datos provenientes del API y confirmamos que funciona.
    
    Se eliminó los {} en weather.js a la propiedad nombre de la ciudad y a la key del api, mostraba un error 401.

--- weather.js
+++ weather.js
@@ -1,3 +1,12 @@
 "use strict"
 
-const url = "http://api.openweathermap.org/data/2.5/weather?q={Santo Domingo}&appid={b9041d1d914c8697e543d1f47bedd975}"
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Santo Domingo&appid=b9041d1d914c8697e543d1f47bedd975";
+
+$.ajax ({
+    url: url,
+    success: function (result){
+        console.log(result);
+        console.log(result.name);
+
+    }
+})
