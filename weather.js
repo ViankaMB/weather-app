c3625de5163e2b7d583803b24e6aad45eece330b
ViankaMB <54475761+ViankaMB@users.noreply.github.com>
2020-04-15 16:12:59 -0400

    Code cleanup
    Limpieza del código

--- weather.js
+++ weather.js
@@ -7,17 +7,22 @@ $.ajax ({
     success: function (result){
         console.log(result);
         console.log(result.name);
+        // Output the location
+        const displayCity = `Current weather in ${result.name}`;
+        $("#location").html(displayCity);
 
-        $("#location").text(result.name);
-
+        //Display the temperature
         const C = Math.round(result.main.temp - 273.15);
-        const Celsius = C.toString();
-        $("#temperature").text(Celsius);
+        const displayTemp = `Temperature: ${C}&#176;C`;
+        $("#temperature").html(displayTemp);
 
+        //Display the wind
         const windSpeed = Math.round(result.wind.speed * 1.60934);
-        const ws = windSpeed.toString();
-        $("#windSpeed").text(ws);
+        const displayWind = `Wind: ${windSpeed} km/h`;
+        $("#windSpeed").html(displayWind);
 
-        $("#sky").text(result.weather[0].description);
+        //Display the sky conditions
+        const displaySkyConditions = `Sky conditions: ${result.weather[0].description}`;
+        $("#sky").html(displaySkyConditions);
     }
 })
